359b03928d0b319a50766a1372779c5f270a7442
jeff <jeff_blaisdell@github.com>
2014-01-14 11:12:45 -0600

    Added in grunt front-end build file.  Corrected jshint warnings.

--- web-app/js/controller/browse.js
+++ web-app/js/controller/browse.js
@@ -2,12 +2,18 @@ define(['app', 'service/post'], function( app ) {
 	'use strict';
 
 	app.controller('BrowseController', ['$scope', 'Posts', function ($scope, Posts) {
+
+		$scope.currentPage = 0;
 		$scope.posts = [];
 
 		Posts.get()
 			.success(function( data ) {
 				$scope.posts = data;
 			});
+
+        $scope.addMoreItems = function () {
+
+		}
 
 	}]);
 });
