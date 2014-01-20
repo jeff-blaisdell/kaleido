26d377cecaf19ba1541c3be28177e84bad57cc7f
jeff <jeff_blaisdell@github.com>
2014-01-19 18:05:43 -0600

    fix jshint warnings.

--- web-app/js/controller/browse.js
+++ web-app/js/controller/browse.js
@@ -4,25 +4,10 @@ define(['app', 'service/post'], function( app ) {
 
 	app.controller('BrowseController', ['$scope', '$state', '$location', 'Posts', function ($scope, $state, $location, Posts) {
 
-		$scope.isPostStateActive = ($state.current.name === 'home.post');
-		$scope.currentPage = 1;
-		$scope.posts = [];
-
-		$scope.addMoreItems = function () {
-			selectPosts($scope.currentPage);
-		};
-
-		$scope.$on('$stateChangeStart', function(evt, toState, toParams, fromState, fromParams) {
-			if ( toState.name === 'home' ) {
-				$scope.isPostStateActive = false;
-				if ( fromState.name === 'home.post' ) {
-					$location.hash(fromParams.postId);
-				}
-			} else if ( toState.name === 'home.post' ) {
-				$scope.isPostStateActive = true;
-			}
-		});
-
+		/**
+		 * Retrieves post from backend.
+		 * @param page The current page of posts the user is on.
+		 */
 		function selectPosts(page) {
 
 			var params = {};
@@ -38,6 +23,41 @@ define(['app', 'service/post'], function( app ) {
 					}
 				});
 		}
+
+		/**
+		 * Event handler for Angular UI-Router's $stateChangeStartEvent.
+		 * The browse page needs to show / hide the infinite scroll
+		 * when a user is viewing the post detail page.  This handler
+		 * encapsulates that logic.
+		 * @see https://github.com/angular-ui/ui-router/wiki#state-change-events for parameter descriptions.
+		 */
+		function onStateChangeStart(evt, toState, toParams, fromState, fromParams) {
+			if ( toState.name === 'home' ) {
+				$scope.isPostStateActive = false;
+				if ( fromState.name === 'home.post' ) {
+					$location.hash(fromParams.postId);
+				}
+			} else if ( toState.name === 'home.post' ) {
+				$scope.isPostStateActive = true;
+			}
+		}
+
+        /**
+         * Callback for infinite-scroll directive used to fetch
+         * next page of content.
+         */
+		function addMoreItems() {
+			selectPosts($scope.currentPage);
+		}
+
+        /**
+         * Initialize $scope.
+         */
+		$scope.isPostStateActive = ($state.current.name === 'home.post');
+		$scope.currentPage = 1;
+		$scope.posts = [];
+		$scope.addMoreItems = addMoreItems;
+		$scope.$on('$stateChangeStart', onStateChangeStart);
 
 	}]);
 });
--- web-app/js/controller/post.js
+++ web-app/js/controller/post.js
@@ -1,14 +1,26 @@
 define(['app'], function( app ) {
+
 	'use strict';
 
 	app.controller('PostController', ['$scope', '$stateParams', 'Posts', function ($scope, $stateParams, Posts) {
 
+		function init() {
+			Posts.get({ 'id' : $stateParams.postId })
+				.success(function( data ) {
+					$scope.post = data;
+				});
+		}
+
+		/**
+		 * Initialize $scope.
+		 */
 		$scope.post = undefined;
 
-		Posts.get({ 'id' : $stateParams.postId })
-			.success(function( data ) {
-				$scope.post = data;
-			});
+
+		/**
+		 * Initialize controller.
+		 */
+		init();
 
 	}]);
 
